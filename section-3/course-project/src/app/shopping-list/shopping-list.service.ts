3d2eae6e30c9483ba176a1566dc8b9b147a758c7
vargavd <vvd007@gmail.com>
2023-08-23 14:59:53 +0200

    Extend course project with Signals

--- section-3/course-project/src/app/shopping-list/shopping-list.service.ts
+++ section-3/course-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,11 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
 import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable()
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -17,7 +19,7 @@ export class ShoppingListService {
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
 
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
@@ -28,6 +30,6 @@ export class ShoppingListService {
     // Spread operator - ES6 feature
     this.ingredients.push(...ingredients);
 
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
